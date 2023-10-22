70389ac491711cb72d2e3664d996ec79d3d4dae0
Marc Escandell <marc.escandell.alonso@estudiantat.upc.edu>
2023-10-22 13:12:15 +0200

    Upgraded PlayerMovement

--- Game/Assets/Scripts/PlayerMovement.cs
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -1,38 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
-enum movDirection
-{
-    IDLE,
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT,
-}
-
 public class PlayerMovement : MonoBehaviour
-{
+{ 
+    public float maxSpeed = 10f;
+    public float acceleration = 0.05f;
 
-    public float moveSpeed = 1f;
+    [SerializeField] private bool isMovingX = false;
+    [SerializeField] private bool isMovingY = false;
 
-    //[SerializeField] private movDirection MovementType = movDirection.IDLE;
+    [SerializeField] private Vector3 directionVector = Vector3.zero;
 
-    // Start is called before the first frame update
-    void Start()
+    void Update()
     {
 
-    }
+        Vector3 auxVec = Vector3.zero;
 
-    // Update is called once per frame
-    void Update()
-    { 
-        Vector3 directionVector = Vector3.zero;
-        if (Input.GetKey(KeyCode.W)) directionVector.y = moveSpeed;
-        if (Input.GetKey(KeyCode.S)) directionVector.y = -moveSpeed;
-        if (Input.GetKey(KeyCode.A)) directionVector.x = -moveSpeed;
-        if (Input.GetKey(KeyCode.D)) directionVector.x = moveSpeed;
+        //Check inputs ---------------------------------------------------------------------------------------------------
+        //--- X axis ---
+        if      (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) { auxVec.x += -acceleration; isMovingX = true; }
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)) { auxVec.x +=  acceleration; isMovingX = true; }
+        else isMovingX = false;
+
+        //--- Y Axis ---
+        if      (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W)) { auxVec.y += -acceleration; isMovingY = true; }
+        else if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)) { auxVec.y +=  acceleration; isMovingY = true; }
+        else isMovingY = false;
+
+        //If no inputs then accelerate to 0 ------------------------------------------------------------------------------
+        if (isMovingX == false)
+        {
+            if (directionVector.x < -acceleration) auxVec.x =  acceleration;
+            if (directionVector.x >  acceleration) auxVec.x = -acceleration;
+        }
+        if (isMovingY == false)
+        {
+            if (directionVector.y < -acceleration) auxVec.y =  acceleration;
+            if (directionVector.y >  acceleration) auxVec.y = -acceleration;
+        }
+
+        //Modify directionVector -----------------------------------------------------------------------------------------
+        directionVector += auxVec;
+
+        //Control that speed doesn't surpass maxSpeed --------------------------------------------------------------------
+        if (directionVector.x >  maxSpeed) directionVector.x =  maxSpeed;
+        if (directionVector.x < -maxSpeed) directionVector.x = -maxSpeed;
+        if (directionVector.y >  maxSpeed) directionVector.y =  maxSpeed;
+        if (directionVector.y < -maxSpeed) directionVector.y = -maxSpeed;
+
+        //Control that speed gets to 0 properly --------------------------------------------------------------------------
+        if (isMovingX == false && directionVector.x > -acceleration && directionVector.x < acceleration) directionVector.x = 0;
+        if (isMovingY == false && directionVector.y > -acceleration && directionVector.y < acceleration) directionVector.y = 0;
 
         transform.position += directionVector * Time.deltaTime;
     }
