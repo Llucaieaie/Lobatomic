8c108f69e7243213f0c8bd01f351121362d7dea9
flishflash <jose.antonio.tur@estudiantat.upc.edu>
2023-10-23 15:22:23 +0200

    tile cambios

--- Game/Assets/Scripts/Tile.cs
+++ Game/Assets/Scripts/Tile.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     public ParticleSystem destroyParticle;
+    public Sprite undisoveredTile, disoveredTile;
+
+    private void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = undisoveredTile;
+    }
 
     public virtual void OnExplosion()
     {
         destroyParticle.Play();
         Destroy(gameObject);
+    }
+
+    public virtual void TileIsDisovered()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = disoveredTile;
     }
 }
