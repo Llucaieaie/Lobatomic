42efaaade483da1eda93b1587f1df7609c66f59c
Lluc <99950000+Llucaieaie@users.noreply.github.com>
2023-10-25 19:01:45 +0200

    barra de vida

--- Game/Assets/Scripts/GeneratePatient.cs
+++ Game/Assets/Scripts/GeneratePatient.cs
@@ -13,41 +13,52 @@ public class GeneratePatient : MonoBehaviour
     public SpriteRenderer baseRenderer;
     public SpriteRenderer expressionRenderer;
 
-    public bool genero;
+    Rect size;
 
-    public int newSortingOrder = 0;
+    public bool genero;
 
     void Start()
     {
-        genero = Random.Range(0, 2) == 0;
-        
         Rect size = new Rect(0, 0, baseLayerM[0].width, baseLayerM[0].height);
-
+        
+        genero = Random.Range(0, 2) == 0;
         if(!genero) //hombre
         {
-            Sprite baseSprite = Sprite.Create(baseLayerM[Random.Range(0, baseLayerM.Length)], new Rect(0, 0, size.width, size.height), Vector2.zero);
+            Sprite baseSprite = Sprite.Create(baseLayerM[Random.Range(0, baseLayerM.Length)], size, Vector2.zero);
             baseRenderer.sprite = baseSprite;
 
-            Sprite hairSprite = Sprite.Create(hairLayerM[Random.Range(0, hairLayerM.Length)], new Rect(0, 0, size.width, size.height), Vector2.zero);
+            Sprite hairSprite = Sprite.Create(hairLayerM[Random.Range(0, hairLayerM.Length)], size, Vector2.zero);
             hairRenderer.sprite = hairSprite;
             hairRenderer.sortingOrder = 2;
-
-            Sprite expressionSprite = Sprite.Create(expressionLayer[Random.Range(0, expressionLayer.Length)], new Rect(0, 0, size.width, size.height), Vector2.zero);
-            expressionRenderer.sprite = expressionSprite;
-
         }
         else //mujer
         {
-            Sprite baseSprite = Sprite.Create(baseLayerF[Random.Range(0, baseLayerF.Length)], new Rect(0, 0, size.width, size.height), Vector2.zero);
+            Sprite baseSprite = Sprite.Create(baseLayerF[Random.Range(0, baseLayerF.Length)], size, Vector2.zero);
             baseRenderer.sprite = baseSprite;
 
-            Sprite hairSprite = Sprite.Create(hairLayerF[Random.Range(0, hairLayerF.Length)], new Rect(0, 0, size.width, size.height), Vector2.zero);
+            Sprite hairSprite = Sprite.Create(hairLayerF[Random.Range(0, hairLayerF.Length)], size, Vector2.zero);
             hairRenderer.sprite = hairSprite;
             hairRenderer.sortingOrder = 0; 
-
-            Sprite expressionSprite = Sprite.Create(expressionLayer[Random.Range(0, expressionLayer.Length)], new Rect(0, 0, size.width, size.height), Vector2.zero);
-            expressionRenderer.sprite = expressionSprite;
         }
+        //expressionRenderer.sprite = Sprite.Create(expressionLayer[1], size, Vector2.zero);
+    }
 
+    public void UpdateExpression(float current)
+    {
+        if (current > 80)
+        {
+            expressionRenderer.sprite = Sprite.Create(expressionLayer[0], size, Vector2.zero);
+            Debug.Log("contento");
+        }
+        else if (current >= 30)
+        {
+            expressionRenderer.sprite = Sprite.Create(expressionLayer[1], size, Vector2.zero);
+            Debug.Log("normal");
+        }
+        else
+        {
+            expressionRenderer.sprite = Sprite.Create(expressionLayer[2], size, Vector2.zero);
+            Debug.Log("loco");
+        }
     }
 }
