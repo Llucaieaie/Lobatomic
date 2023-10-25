ac07c4d2dd4e661515c357f554be9c41a8717b9b
Lluc <99950000+Llucaieaie@users.noreply.github.com>
2023-10-25 15:31:45 +0200

    marc fes pull abans de push xd

--- Game/Assets/Lluc/happinessBar.cs
+++ Game/Assets/Lluc/happinessBar.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class happinessBar : MonoBehaviour
+public class HappinessBar : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image bar;
+    public TextMeshProUGUI percent;
+
+    private int min;
+    private int max;
+    public float current;
+    public string percentText;
+
     void Start()
     {
-        
+        min = 0;
+        max = 100;
+        current = Random.Range(33, 46);
+        UpdateBar();
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void UpdateBar()
+    {
+        bar.fillAmount = current / 100;
+        percentText = string.Format("{0}%", current);
+        percent.text = percentText;
+    }
+
+    public void destroySadTile()
+    {
+        current += 6;
+        if (current > max) { current = max; }
+        UpdateBar();
+    }
+    public void destroyHappyTile()
+    {
+        current -= 8;
+        if (current < min) { current = min; }
+        UpdateBar();
     }
 }
