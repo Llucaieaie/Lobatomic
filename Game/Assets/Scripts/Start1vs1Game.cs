56c239165ae6f931fc7fb10449ff8e167005695e
Lluc <99950000+Llucaieaie@users.noreply.github.com>
2024-12-16 21:04:44 +0100

    Start 1vs1 game WIP

---
+++ Game/Assets/Scripts/Start1vs1Game.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Start1vs1Game : MonoBehaviour
+{
+    private Button startButton;
+    public LobbyManager lobbyManager;
+
+    private void Start()
+    {
+        startButton = GetComponent<Button>();
+        startButton.interactable = false;
+    }
+
+    private void Update()
+    {
+        if (lobbyManager.Player1.GetComponent<PlayerDataManager>().isControlled && lobbyManager.Player2.GetComponent<PlayerDataManager>().isControlled)
+        {
+            startButton.interactable = true;
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("VersusScene");
+    }
+}
